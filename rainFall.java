d22b4be7891d9e0bbb7bf8c41755f5e9654c1641
Lemuel <lemueluhuru@gmail.com>
2013-05-03 13:19:19 -0400

    Add rainFall class

---
+++ rainFall.java
@@ -0,0 +1,16 @@
+/*
+ * @Author Wayne A. Tatum, Jr.
+ * @Date   May 2, 2013
+ * 
+ * Program that creates a class and stores the total 
+ * rainfall for each 12 months into an array of doubles 
+ * and demonstrates the the class in a complete program.
+ */
+
+public class rainFall {
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
