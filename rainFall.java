71bf7341b6053f17cdd026eb9abc7fe82091ab47
Lemuel <lemueluhuru@gmail.com>
2013-05-04 18:45:03 -0400

    Add functions that return most and least rain fall - Complete rainFall class assignment.

--- rainFall.java
+++ rainFall.java
@@ -23,6 +23,8 @@ public class rainFall {
 		
 		System.out.println("The annual sum of rain: " + sumRain(monthlyRain));
 		System.out.println("The average sum of rain: " + averageRain(monthlyRain));
+		System.out.println("The most amount of rain fall: " + mostRain(monthlyRain));
+		System.out.println("The least amount of rain fall: " + leastRain(monthlyRain));
 	}		
 	
 	// Create a function that returns the total rainFall for the year.
@@ -45,14 +47,24 @@ public class rainFall {
 	// Create a function that returns the month with the most rain.
 	static double mostRain(double[] array){
 		double most = 0;
-		for (int i = 0; i < array; i++){
-			if(array[i] > array[i+1]){
-				most = 
+		for (int i = 0; i < array.length; i++){
+			if(array[i] > most){
+				most = array[i];
 			}
 		}
+		return most;
 	}
 	
 	// Create a function that returns the month with the least rain.
+	static double leastRain(double[] array){
+		double least = array[0];
+		for(int i = 0; i < array.length; i++){
+			if(array[i] < least){
+				least = array[i];
+			}
+		}
+		return least;
+	}
 	
 	
 				
