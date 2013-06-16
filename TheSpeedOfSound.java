881bab424a2b10a4c38579f3285aaea8f7e6d814
Lemuel <lemueluhuru@gmail.com>
2013-06-16 01:57:59 -0400

    Update TheSpeedOfSound class

--- TheSpeedOfSound.java
+++ TheSpeedOfSound.java
@@ -1,4 +1,5 @@
 import javax.swing.JOptionPane;
+import java.text.*;
 
 /**@author Lemuel M. Uhuru
  * @Date   June 15, 2013
@@ -14,42 +15,49 @@ public class TheSpeedOfSound {
 	
 	public static void main(String[] args) {
 		
-		// Declare constants that approximate the
-				// speed of sound in Air, Steel, and Water
+		//Declare constants that approximate the
+		//speed of sound in Air, Steel, and Water
 				int AIR = 1100;
 				int WATER = 4900;
 				int STEEL = 16400;
 			
-		// Declare var for user input
+		//Declare variable to hold medium type and distance value.
 		String medium,
 		       distance;
-		
-		
 	    
-	    // Prompt user for medium
-	    medium = JOptionPane.showInputDialog("Enter the medium (water/steel/air) ");
+	    //Prompt user for medium
+	    medium = JOptionPane.showInputDialog("Enter the medium (water/steel/air) ").toLowerCase();
 	    
-	    // Prompt user for distance
+	    //Prompt user for distance
 	   distance = JOptionPane.showInputDialog("Enter distance in feet");
 	    
-	    // Convert distance to data-type double
-	    double D = Double.parseDouble(distance);
+	    //Convert distance to data-type double
+	    double D  = Double.parseDouble(distance);
 	    
-	 // Declare variable to hold time
-	    double wTime =  D/WATER;
-	    double aTime = D/AIR;
-	    double sTime = D/STEEL;
+	    // Calculate time for sound to travel through medium
+	    double timeA = D/AIR;
+	    double timeW = D/WATER;
+	    double timeS = D/STEEL;
 	    
-	    if(medium == "water"){
-	    	JOptionPane.showMessageDialog(null, "Medium: " + medium + "\nDistance: " + D + "\nTime: " + wTime);
-	    }
-	    else if(medium == "air"){
-	    	JOptionPane.showMessageDialog(null, "Medium: " + medium + "\nDistance: " + D + "\nTime: " + aTime);
-	    }
-	    else if(medium == "steel"){
-	    	JOptionPane.showMessageDialog(null, "Medium: " + medium + "\nDistance: " + D + "\nTime: " + sTime);
-	    }
-	    
-	}
+	    // Decimal formating for Time variables
+	    NumberFormat df = DecimalFormat.getInstance();
+	    df.setMaximumFractionDigits(2);
 
+	    
+	    //Design if-else control structure to display the amount
+	    //of time it will take sound to travel through the medium given 
+	    //the users input.
+	    
+	 // *Don't understand why the '==' or equal to operator cannot be used 
+	//  as a substitute for the equals method in the following conditions.
+	    
+	    if(medium.equals("water"))
+	    	JOptionPane.showMessageDialog(null, "Medium: " + medium + "\nDistance: " + D + " ft" + "\nTime: " + df.format(timeW) + " seconds");
+	    else if(medium.equals("air"))
+	    	JOptionPane.showMessageDialog(null, "Medium: " + medium + "\nDistance: " + D + " ft" + "\nTime: " + df.format(timeA) + " seconds");
+	    else if(medium.equals("steel"))
+	    	JOptionPane.showMessageDialog(null, "Medium: " + medium + "\nDistance: " + D + " ft" + "\nTime: " + df.format(timeS) + " seconds");
+	    else
+	    	JOptionPane.showMessageDialog(null, "Invalid input");
+	}
 }
