ca978fbfbcf07b2cc25539e8b11ac7006ab6fb31
Lemuel <lemueluhuru@gmail.com>
2013-06-21 23:57:01 -0400

    Update SumOfNumbers class

--- SumOfNumbers.java
+++ SumOfNumbers.java
@@ -15,16 +15,20 @@ public class SumOfNumbers {
 
 	public static void main(String[] args) {
 		// Declare variable to hold user input
-		
+		int num;
 		// Prompt user for positive nonzero integer value and store in variable.
 		String input1 = JOptionPane.showInputDialog("Enter a positive nonzero integer");
 		
-		// Convert input1 to an integer data-type
-		int num = Integer.parseInt(input1);
+		// Convert input1 to data-type int
+		num = Integer.parseInt(input1);
 		
-		// Construct while loop to obtain the sum of integers from 1 up to the users input.
+		// Declare variable to hold sum
+		int sum = 0;
 		
-
+		// Construct for loop to obtain the sum of integers from 1 up to the users input.
+		while(num > 1){
+			num--;
+		}
+		
 	}
-
 }
