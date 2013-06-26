f4d539810bde243df58ee27d235a8362480ed551
Lemuel <lemueluhuru@gmail.com>
2013-06-25 22:29:20 -0400

    Add LetterCounter class

---
+++ LetterCounter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+/**@author Lemuel M. Uhuru
+ * @Date   June 25, 2013
+ * Book Title: From Control Structures through Objects by Tony Gaddis 
+ * Page: 267 Challenge 5: Letter Counter
+ * 
+ * @Description Write a program that asks the user to enter a string, and then asks
+ * the user to enter a character. The program should count and display the number of 
+ * times that the specified character appears in the string.
+ */
+
+public class LetterCounter {
+
+	public static void main(String[] args) {
+
+	}
+
+}
