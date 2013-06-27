95018385219b1d751404ade1d3f4cf69767f0f0b
Lemuel <lemueluhuru@gmail.com>
2013-06-26 23:58:42 -0400

    Add FileLetterCounter class

---
+++ FileLetterCounter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+/**@author Lemuel M. Uhuru
+ * @Date   June 26, 2013
+ * Book Title: From Control Structures through Objects by Tony Gaddis 
+ * Page: 267 Challenge 6: File Letter Counter
+ * 
+ * @Description Write a program that asks the user to enter the name of a file, 
+ * and then asks the user to enter a character. The program should count and display
+ * the number of times that the specified character appears in the file. Use Notepad or
+ * another text editor to create a s simple file that can be used to test the program.
+ */
+
+public class FileLetterCounter {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
