f8a2e8396cca07a0bc853f0607440a30985d2087
Lemuel <lemueluhuru@gmail.com>
2013-06-27 23:09:37 -0400

    Add HotelOccupancy class

---
+++ HotelOccupancy.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**@author Lemuel M. Uhuru
+ * @Date   June 27, 2013
+ * Book Title: From Control Structures through Objects by Tony Gaddis 
+ * Page: 268 Challenge 7: Hotel Occupancy
+ * 
+ * @Description Write a program that calculates the occupancy rate for each 
+ * floor of a hotel. The program should start by asking for the number of floors
+ * in the hotel. A loop should then iterate once for each floor. During each iteration,
+ * the loop should ask the user for the number of rooms on the floor and the number of 
+ * them that are occupied. After all the iterations, the program should display the number
+ * of rooms the hotel has, the number of them that are occupied, the number that are vacant,
+ * and the occupancy rate for the hotel. Do not accept a value less than one for the number
+ * of floors. Do not accept a number less than 10 for the number of rooms on a floor.
+ */
+
+
+public class HotelOccupancy {
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
