1a6308c7b6ee6382d215ac103893f2918eb3512a
Lemuel <lemueluhuru@gmail.com>
2013-07-02 18:56:54 -0400

    Complete FileLetterCounter class

--- FileLetterCounter.java
+++ FileLetterCounter.java
@@ -16,30 +16,54 @@ public class FileLetterCounter {
 
 	public static void main(String[] args) throws IOException{
 		
-		// Create Scanner object
+		// Create Scanner object for user input
 		Scanner input = new Scanner(System.in);
 		
-		// Prompt user for file name and store value in fileName
-		System.out.println("Enter file name: ");
+		// Prompt user for file name 
+		System.out.print("Enter file name: ");
 		String fileName = input.nextLine();
+
+		// Create File 
+		File file = new File(fileName + ".txt");
 		
+		// Ensure that file exist
+		if(!file.exists()){
+			System.out.println("The file " + fileName + ".txt does not exist.");
+			System.exit(0);
+		}
+		
+		// Create scanner object to open file.
+		Scanner fileInput = new Scanner(file);
+		
+				
 		// Prompt user for the specified character and store in variable
 		System.out.println("Enter a single character: ");
 		String charInput = input.nextLine();
 		char character = charInput.charAt(0);
 		
-		// Create File and Scanner object to open the file.
-		File file = new File(fileName + ".txt");
-		Scanner fileInput = new Scanner(file);
 		
 		// Declare var to store lines from the file
-		String line = fileInput.nextLine();
+		String line; 
 		
 		// Declare counting variable
 		int count = 0;
 		
-		// Construct for loop to calculate the number of times
-		// the specified character appears within the file.
-		System.out.println();
+		// Construct while and for loop to calculate the number of 
+		// times the specified character appears within the file.
+		
+		while(fileInput.hasNext()){
+			line = fileInput.nextLine();
+			for(int j=0; j<line.length(); j++){
+				if(line.charAt(j)==character){
+					count += 1;
+				}
+			}
+		}
+		
+		// Diplay character count
+		System.out.println("The character " + character + " appears in the file " + count + " times.");
+		
+		// Close file
+		fileInput.close();
 	}
 }
